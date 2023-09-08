4461d4fa216973a13e79030091e535d15b0676ca
Sufferal <botnari_ciprian@mail.ru>
2023-09-08 12:11:00 +0300

    Fix navbar initial state

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  menuDisplay: boolean = true;
+  menuDisplay: boolean = false;
 
   toggleMenu() {
     this.menuDisplay = !this.menuDisplay;   
