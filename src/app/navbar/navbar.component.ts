4eb9e83d76dde7741680365c000097a3ec6f8151
Ciprian <48862508+Sufferal@users.noreply.github.com>
2024-01-27 10:27:03 +0200

    fix: navbar styles (#4)

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  menuDisplay: boolean = false;
+  menuDisplay: boolean = true;
 
   toggleMenu() {
     this.menuDisplay = !this.menuDisplay;   
