e9fd26c9ec4eeff6bc1d3d36799c7c119ab847e6
Sufferal <botnari_ciprian@mail.ru>
2024-02-03 11:34:56 +0200

    enhancement: add header effect

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  particles: any = []
 
+  ngOnInit() {
+    this.particles = Array(100).fill(0);
+  }
 }
